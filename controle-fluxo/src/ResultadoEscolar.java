74d6e332e5ac80df12d4805e9f4063a41346e9c2
felipe <felipe.impt@hotmail.com>
2023-09-22 18:46:29 -0300

    2209

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+
+        int nota = 7; 
+
+        if(nota >= 8)
+        System.out.println("Aprovado");
+
+        else
+        System.out.println("Reprovado");
+    }
+}
